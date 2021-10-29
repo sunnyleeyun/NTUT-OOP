b2c94c7e352c56ab169d9c6d1ae4fa2f637e2848
sunnyleeyun <sunnylee945@hotmail.com>
2021-10-29 20:26:47 +0800

    Fix HW1 indentation

--- HW01/src/dot.h
+++ HW01/src/dot.h
@@ -8,19 +8,18 @@
 //       then dot(u, 2, v, 2) = 11
 
 double dot(double *u, int dim_u, double *v, int dim_v) {
+    // shouldn't have different dimensions and negative dimension
+    if (dim_u != dim_v || dim_u < 0 || dim_v < 0) {
+        throw std::string("undefined.");
+    }
+	
+    // compute the inner product of two vectors
+    double result = 0;
+    for (int i = 0; i < dim_u; i++) {
+        result += (*(u + i) * *(v + i));
+    }
 
-  	// shouldn't have different dimensions and negative dimension
-	if (dim_u != dim_v || dim_u < 0 || dim_v < 0) {
-    	throw std::string("undefined.");
-  	}
-
- 	// compute the inner product of two vectors
-  	double result = 0;
-  	for (int i = 0; i < dim_u; i++) {
-    	result += (*(u + i) * *(v + i));
-  	}
-
-  	return result;
+    return result;
 } // end of dot
 
 #endif // end of include guard: DOT_H
